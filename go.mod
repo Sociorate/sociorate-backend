082bd081bc6da1b3b7ff1158fe954c7668f5fec7
nickname32 <41116859+nickname32@users.noreply.github.com>
2020-10-19 16:46:37 +0300

    test

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Sociorate/sociorate-backend
+
+go 1.15
+
+require github.com/valyala/fasthttp v1.16.0
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/valyala/fasthttp"
+)
+
+var vkSecretKey = os.Getenv("VK_SECRET_KEY")
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		panic("$PORT must be set")
+	}
+
+	err := fasthttp.ListenAndServe(":"+port, requestHandler)
+	if err != nil {
+		panic(fmt.Errorf("Error in ListenAndServe: %s", err))
+	}
+}
+
+func requestHandler(ctx *fasthttp.RequestCtx) {
+	switch string(ctx.Method()) {
+	case "GET":
+		ctx.WriteString("{ data: { rating: [1, 2, 4, 9, 15] } }")
+	case "POST":
+		ctx.WriteString("{}")
+	}
+
+	ctx.SetContentType("application/json; charset=utf8")
+}
