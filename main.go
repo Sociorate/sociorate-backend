12f655cb38371929a898133d80e6a024b336bbd3
nickname32 <41116859+nickname32@users.noreply.github.com>
2020-10-22 15:36:33 +0300

    okj

--- main.go
+++ main.go
@@ -78,9 +78,10 @@ var (
 )
 
 const createUsersTableSQL = `CREATE TABLE IF NOT EXISTS users (
-	vk_userid INTEGER NOT NULL PRIMARY KEY,
-	rating_counts INTEGER[5][7] NOT NULL,
-	rating_dates DATE[7] NOT NULL);`
+	vk_userid INTEGER NOT NULL PRIMARY KEY
+	last_rate_time TIMESTAMP,
+	rating_counts INTEGER[5][7],
+	rating_dates DATE[7]);`
 
 func main() {
 	dbconn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
