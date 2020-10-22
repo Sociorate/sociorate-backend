a540c9cbd5a7ab4bfc41feccb46982e4a4a7ee07
nickname32 <41116859+nickname32@users.noreply.github.com>
2020-10-22 14:40:37 +0300

    q

--- main.go
+++ main.go
@@ -79,8 +79,8 @@ var (
 
 const createUsersTableSQL = `CREATE TABLE IF NOT EXISTS users (
 	vk_userid INT NOT NULL PRIMARY KEY,
-	ratingCounts integer[5][7]
-	ratingDates date[7]
+	ratingCounts integer[5][7],
+	ratingDates date[7],
 );`
 
 func main() {
